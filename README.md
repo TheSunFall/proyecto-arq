e9f0dcefcbc5fabbd77494dd42cc577f1451d3b0
Juan Aguilar <juan.aguilarl@unmsm.edu.pe>
2024-12-10 03:15:22 -0500

    arreglo en la navegación

--- README.md
+++ README.md
@@ -7,7 +7,11 @@
 ## Instalación
 
 * Clonar o descargar el repositorio
-* Para usar la api de Google Sheets, crear un proyecto de Google, renombrar el archivo descargado a "credenciales.json" y moverlo a esta carpeta (aún no implementado)
+* Para usar la api de Google Sheets, crear un proyecto de Google y descargar el archivo de credenciales. Luego, en la raíz del proyecto crear un archivo llamado .env.local con los siguientes campos:
+    *  GOOGLE_PRIVATE_KEY= Del campo "private_key" en el archivo de credenciales, todo lo que está entre `"-----BEGIN PRIVATE KEY-----"` y `"-----END PRIVATE KEY-----#`
+    * GOOGLE_CLIENT_EMAIL= El valor del campo "client_email"
+    * GOOGLE_CLIENT_ID= El valor del campo "client_id"
+    * SPREADSHEET_ID= En la URL de la hoja de cálculo, el valor entre /d/ y /edit
 * Ejecutar `npm install`
 * Ejecutar `npm run dev`
 
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,11 +1,13 @@
+import Link from "next/link"
+
 export default function Navbar() {
     return (
         <nav className="flex text-slate-100 bg-gray-950 h-fit">
             <h1 className="mr-auto font-semibold text-center bg-pink-800 p-4 pl-12 pr-12 h-full">Sistema de Riego</h1>
             <div className="h-full self-center">
-                <a className="bg-pink-950 hover:bg-pink-800 active:font-bold active:bg-pink-600 p-4 pr-6 pl-6 h-full" href="/">Inicio</a>
-                <a className="bg-pink-950 hover:bg-pink-800 active:font-bold active:bg-pink-600 p-4 pr-6 pl-6 h-full" href="/controles">Controles</a>
-                <a className="bg-pink-950 hover:bg-pink-800 active:font-bold active:bg-pink-600 p-4 pr-6 pl-6 h-full" href="/historial">Historial</a>
+                <Link className="bg-pink-950 hover:bg-pink-800 active:font-bold active:bg-pink-600 p-4 pr-6 pl-6 h-full" href="/">Inicio</Link>
+                <Link className="bg-pink-950 hover:bg-pink-800 active:font-bold active:bg-pink-600 p-4 pr-6 pl-6 h-full" href="/controles">Controles</Link>
+                <Link className="bg-pink-950 hover:bg-pink-800 active:font-bold active:bg-pink-600 p-4 pr-6 pl-6 h-full" href="/historial">Historial</Link>
             </div>
         </nav>
     )
